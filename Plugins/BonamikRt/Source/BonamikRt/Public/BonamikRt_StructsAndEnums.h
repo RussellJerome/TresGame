b2d7177113f577414eeeb5b5266f5dfd99c1c2bc
Joseph Lee <17460257+josephl70@users.noreply.github.com>
2021-04-07 02:44:50 -0400

    Update BonamikRt_StructsAndEnums.h

--- Plugins/BonamikRt/Source/BonamikRt/Public/BonamikRt_StructsAndEnums.h
+++ Plugins/BonamikRt/Source/BonamikRt/Public/BonamikRt_StructsAndEnums.h
@@ -12,161 +12,161 @@
 UENUM(BlueprintType)
 enum ESHAPEMATCHING_MODE
 {
-	ESQEX_Bonamik_SMMODE_CHAINSHAPEMATCH = 0,
-	ESQEX_Bonamik_SMMODE_SEQCHAINMATCH = 1,
-	ESQEX_Bonamik_SMMODE_VOLUMEMATCH = 2,
-	ESQEX_Bonamik_SMMODE_MAX = 3
+	ESQEX_Bonamik_SMMODE_CHAINSHAPEMATCH = 0 UMETA(DisplayName = "Chain Shape Match"),
+	ESQEX_Bonamik_SMMODE_SEQCHAINMATCH = 1 UMETA(DisplayName = "SEQ Chain Match"),
+	ESQEX_Bonamik_SMMODE_VOLUMEMATCH = 2 UMETA(DisplayName = "Volume Match"),
+	ESQEX_Bonamik_SMMODE_MAX = 3 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_FastMotionHandling
 {
-	ESQEX_Bonamik_FMH_NONE = 0,
-	ESQEX_Bonamik_FMH_LOCAL_FORCE = 1,
-	ESQEX_Bonamik_FMH_LSS = 2,
-	ESQEX_Bonamik_FMH_MAX = 3
+	ESQEX_Bonamik_FMH_NONE = 0 UMETA(DisplayName = "None"),
+	ESQEX_Bonamik_FMH_LOCAL_FORCE = 1 UMETA(DisplayName = "Local Force"),
+	ESQEX_Bonamik_FMH_LSS = 2 UMETA(DisplayName = "LSS"),
+	ESQEX_Bonamik_FMH_MAX = 3 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_JointLinkSimulationMode
 {
-	ESQEX_Bonamik_JOINTLINK_MODE_SEQ = 0,
-	ESQEX_Bonamik_JOINTLINK_MODE_BATCH = 1,
-	ESQEX_Bonamik_JOINTLINK_MODE_MAX = 2
+	ESQEX_Bonamik_JOINTLINK_MODE_SEQ = 0 UMETA(DisplayName = "SEQ"),
+	ESQEX_Bonamik_JOINTLINK_MODE_BATCH = 1 UMETA(DisplayName = "Batch"),
+	ESQEX_Bonamik_JOINTLINK_MODE_MAX = 2 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_ConstraintType
 {
-	ESQEX_Bonamik_ConstraintType_None = 0,
-	ESQEX_Bonamik_ConstraintType_Sphere = 1,
-	ESQEX_Bonamik_ConstraintType_Spring = 2,
-	ESQEX_Bonamik_ConstraintType_GroundAttach = 3,
-	ESQEX_Bonamik_ConstraintType_SecondOrder_Sphere = 4,
-	ESQEX_Bonamik_ConstraintType_MAX = 5
+	ESQEX_Bonamik_ConstraintType_None = 0 UMETA(DisplayName = "None"),
+	ESQEX_Bonamik_ConstraintType_Sphere = 1 UMETA(DisplayName = "Sphere"),
+	ESQEX_Bonamik_ConstraintType_Spring = 2 UMETA(DisplayName = "Spring"),
+	ESQEX_Bonamik_ConstraintType_GroundAttach = 3 UMETA(DisplayName = "Ground Attach"),
+	ESQEX_Bonamik_ConstraintType_SecondOrder_Sphere = 4 UMETA(DisplayName = "Second Order Sphere"),
+	ESQEX_Bonamik_ConstraintType_MAX = 5 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_CollisionShape
 {
-	ESQEX_Bonamik_CollisionShape_None = 0,
-	ESQEX_Bonamik_CollisionShape_Sphere = 1,
-	ESQEX_Bonamik_CollisionShape_Capsule = 2,
-	ESQEX_Bonamik_CollisionShape_Inverse_Sphere = 3,
-	ESQEX_Bonamik_CollisionShape_Inverse_Capsule = 4,
-	ESQEX_Bonamik_CollisionShape_ReservedA = 5,
-	ESQEX_Bonamik_CollisionShape_Plane = 6,
-	ESQEX_Bonamik_CollisionShape_ReservedB = 7,
-	ESQEX_Bonamik_CollisionShape_Box = 8,
-	ESQEX_Bonamik_CollisionShape_Self_Collision = 9,
-	ESQEX_Bonamik_CollisionShape_MAX = 10
+	ESQEX_Bonamik_CollisionShape_None = 0 UMETA(DisplayName = "None"),
+	ESQEX_Bonamik_CollisionShape_Sphere = 1 UMETA(DisplayName = "Sphere"),
+	ESQEX_Bonamik_CollisionShape_Capsule = 2 UMETA(DisplayName = "Capsule"),
+	ESQEX_Bonamik_CollisionShape_Inverse_Sphere = 3 UMETA(DisplayName = "Inverse Sphere"),
+	ESQEX_Bonamik_CollisionShape_Inverse_Capsule = 4 UMETA(DisplayName = "Inverse Capsule"),
+	ESQEX_Bonamik_CollisionShape_ReservedA = 5 UMETA(DisplayName = "Reserved A"),
+	ESQEX_Bonamik_CollisionShape_Plane = 6 UMETA(DisplayName = "Plane"),
+	ESQEX_Bonamik_CollisionShape_ReservedB = 7 UMETA(DisplayName = "Reserved B"),
+	ESQEX_Bonamik_CollisionShape_Box = 8 UMETA(DisplayName = "Box"),
+	ESQEX_Bonamik_CollisionShape_Self_Collision = 9 UMETA(DisplayName = "Self Collision"),
+	ESQEX_Bonamik_CollisionShape_MAX = 10 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_LinkType
 {
-	ESQEX_Bonamik_LinkType_Lateral = 0,
-	ESQEX_Bonamik_LinkType_ParentChild = 1,
-	ESQEX_Bonamik_LinkType_Bending = 2,
-	ESQEX_Bonamik_LinkType_PinConstraint = 3,
-	ESQEX_Bonamik_LinkType_KdiConstraint = 4,
-	ESQEX_Bonamik_LinkType_ReactionTransfer = 5,
-	ESQEX_Bonamik_LinkType_InverseFreeConstraint = 6,
-	ESQEX_Bonamik_LinkType_MAX = 7
+	ESQEX_Bonamik_LinkType_Lateral = 0 UMETA(DisplayName = "Lateral"),
+	ESQEX_Bonamik_LinkType_ParentChild = 1 UMETA(DisplayName = "Parent Child"),
+	ESQEX_Bonamik_LinkType_Bending = 2 UMETA(DisplayName = "Bending"),
+	ESQEX_Bonamik_LinkType_PinConstraint = 3 UMETA(DisplayName = "Pin Constraint"),
+	ESQEX_Bonamik_LinkType_KdiConstraint = 4 UMETA(DisplayName = "Kdi Constraint"),
+	ESQEX_Bonamik_LinkType_ReactionTransfer = 5 UMETA(DisplayName = "Reaction Transfer"),
+	ESQEX_Bonamik_LinkType_InverseFreeConstraint = 6 UMETA(DisplayName = "Inverse Free Constraint"),
+	ESQEX_Bonamik_LinkType_MAX = 7 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_OutOfCamera
 {
-	ESQEX_Bonamik_OutOfCamera_Default = 0,
-	ESQEX_Bonamik_OutOfCamera_Override = 1,
-	ESQEX_Bonamik_OutOfCamera_FullCheck = 2,
-	ESQEX_Bonamik_OutOfCamera_UseGlobal = 3,
-	ESQEX_Bonamik_OutOfCamera_MAX = 4
+	ESQEX_Bonamik_OutOfCamera_Default = 0 UMETA(DisplayName = "Default"),
+	ESQEX_Bonamik_OutOfCamera_Override = 1 UMETA(DisplayName = "Override"),
+	ESQEX_Bonamik_OutOfCamera_FullCheck = 2 UMETA(DisplayName = "Full Check"),
+	ESQEX_Bonamik_OutOfCamera_UseGlobal = 3 UMETA(DisplayName = "Use Global"),
+	ESQEX_Bonamik_OutOfCamera_MAX = 4 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_Parameters
 {
-	ESQEX_Bonamik_Parameters_Mass = 0,
-	ESQEX_Bonamik_Parameters_Friction = 1,
-	ESQEX_Bonamik_Parameters_Damping = 2,
-	ESQEX_Bonamik_Parameters_Bounciness = 3,
-	ESQEX_Bonamik_Parameters_WindDrag = 4,
-	ESQEX_Bonamik_Parameters_WindLift = 5,
-	ESQEX_Bonamik_Parameters_LFCoeff = 6,
-	ESQEX_Bonamik_Parameters_ParkRate = 7,
-	ESQEX_Bonamik_Parameters_MaxLF = 8,
-	ESQEX_Bonamik_Parameters_LFDrag = 9,
-	ESQEX_Bonamik_Parameters_LFLift = 10,
-	ESQEX_Bonamik_Parameters_InnerConeStr = 11,
-	ESQEX_Bonamik_Parameters_OuterConeStr = 12,
-	ESQEX_Bonamik_Parameters_MAX = 13
+	ESQEX_Bonamik_Parameters_Mass = 0 UMETA(DisplayName = "Mass"),
+	ESQEX_Bonamik_Parameters_Friction = 1 UMETA(DisplayName = "Friction"),
+	ESQEX_Bonamik_Parameters_Damping = 2 UMETA(DisplayName = "Damping"),
+	ESQEX_Bonamik_Parameters_Bounciness = 3 UMETA(DisplayName = "Bounciness"),
+	ESQEX_Bonamik_Parameters_WindDrag = 4 UMETA(DisplayName = "Wind Drag"),
+	ESQEX_Bonamik_Parameters_WindLift = 5 UMETA(DisplayName = "Wind Lift"),
+	ESQEX_Bonamik_Parameters_LFCoeff = 6 UMETA(DisplayName = "LF Coeff"),
+	ESQEX_Bonamik_Parameters_ParkRate = 7 UMETA(DisplayName = "Park Rate"),
+	ESQEX_Bonamik_Parameters_MaxLF = 8 UMETA(DisplayName = "Max LF"),
+	ESQEX_Bonamik_Parameters_LFDrag = 9 UMETA(DisplayName = "LF Drag"),
+	ESQEX_Bonamik_Parameters_LFLift = 10 UMETA(DisplayName = "LF Lift"),
+	ESQEX_Bonamik_Parameters_InnerConeStr = 11 UMETA(DisplayName = "Inner Cone Str"),
+	ESQEX_Bonamik_Parameters_OuterConeStr = 12 UMETA(DisplayName = "Outer Cone Str"),
+	ESQEX_Bonamik_Parameters_MAX = 13 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_GlobalOutOfCamera
 {
-	ESQEX_Bonamik_GlobalOutOfCamera_Default = 0,
-	ESQEX_Bonamik_GlobalOutOfCamera_Override = 1,
-	ESQEX_Bonamik_GlobalOutOfCamera_FullCheck = 2,
-	ESQEX_Bonamik_GlobalOutOfCamera_MAX = 3
+	ESQEX_Bonamik_GlobalOutOfCamera_Default = 0 UMETA(DisplayName = "Default"),
+	ESQEX_Bonamik_GlobalOutOfCamera_Override = 1 UMETA(DisplayName = "Override"),
+	ESQEX_Bonamik_GlobalOutOfCamera_FullCheck = 2 UMETA(DisplayName = "Full Check"),
+	ESQEX_Bonamik_GlobalOutOfCamera_MAX = 3 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_EmissiveDirection
 {
-	ESQEX_Bonamik_EmissiveDirection_UpVec = 0,
-	ESQEX_Bonamik_EmissiveDirection_ModelSpace = 1,
-	ESQEX_Bonamik_EmissiveDirection_WorldSpace = 2,
-	ESQEX_Bonamik_EmissiveDirection_MAX = 3
+	ESQEX_Bonamik_EmissiveDirection_UpVec = 0 UMETA(DisplayName = "Up Vec"),
+	ESQEX_Bonamik_EmissiveDirection_ModelSpace = 1 UMETA(DisplayName = "Model Space"),
+	ESQEX_Bonamik_EmissiveDirection_WorldSpace = 2 UMETA(DisplayName = "World Space"),
+	ESQEX_Bonamik_EmissiveDirection_MAX = 3 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_WindOwnerType
 {
-	ESQEX_Bonamik_WindOwnerType_Actor = 0,
-	ESQEX_Bonamik_WindOwnerType_Preset = 1,
-	ESQEX_Bonamik_WindOwnerType_AnimNotify = 2,
-	ESQEX_Bonamik_WindOwnerType_Matinee = 3,
-	ESQEX_Bonamik_WindOwnerType_AllTypes = 4,
-	ESQEX_Bonamik_WindOwnerType_MAX = 5
+	ESQEX_Bonamik_WindOwnerType_Actor = 0 UMETA(DisplayName = "Actor"),
+	ESQEX_Bonamik_WindOwnerType_Preset = 1 UMETA(DisplayName = "Preset"),
+	ESQEX_Bonamik_WindOwnerType_AnimNotify = 2 UMETA(DisplayName = "Anim Notify"),
+	ESQEX_Bonamik_WindOwnerType_Matinee = 3 UMETA(DisplayName = "Manitee"),
+	ESQEX_Bonamik_WindOwnerType_AllTypes = 4 UMETA(DisplayName = "All Types"),
+	ESQEX_Bonamik_WindOwnerType_MAX = 5 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_WindAreaType
 {
-	ESQEX_Bonamik_WindAreaType_Global = 0,
-	ESQEX_Bonamik_WindAreaType_Box = 1,
-	ESQEX_Bonamik_WindAreaType_Sphere = 2,
-	ESQEX_Bonamik_WindAreaType_MAX = 3
+	ESQEX_Bonamik_WindAreaType_Global = 0 UMETA(DisplayName = "Global"),
+	ESQEX_Bonamik_WindAreaType_Box = 1 UMETA(DisplayName = "Box"),
+	ESQEX_Bonamik_WindAreaType_Sphere = 2 UMETA(DisplayName = "Sphere"),
+	ESQEX_Bonamik_WindAreaType_MAX = 3 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_WindType_Notify
 {
-	ESQEX_Bonamik_WindType_Notify_Directional = 0,
-	ESQEX_Bonamik_WindType_Notify_PointBlast = 1,
-	ESQEX_Bonamik_WindType_Notify_MAX = 2
+	ESQEX_Bonamik_WindType_Notify_Directional = 0 UMETA(DisplayName = "Directional"),
+	ESQEX_Bonamik_WindType_Notify_PointBlast = 1 UMETA(DisplayName = "Point Blast"),
+	ESQEX_Bonamik_WindType_Notify_MAX = 2 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_CustomWindType
 {
-	ESQEX_Bonamik_CustomWindType_Directional = 0,
-	ESQEX_Bonamik_CustomWindType_Wave = 1,
-	ESQEX_Bonamik_CustomWindType_PointBlast = 2,
-	ESQEX_Bonamik_CustomWindType_MAX = 3
+	ESQEX_Bonamik_CustomWindType_Directional = 0 UMETA(DisplayName = "Directional"),
+	ESQEX_Bonamik_CustomWindType_Wave = 1 UMETA(DisplayName = "Wave"),
+	ESQEX_Bonamik_CustomWindType_PointBlast = 2 UMETA(DisplayName = "Point Blast"),
+	ESQEX_Bonamik_CustomWindType_MAX = 3 UMETA(DisplayName = "MAX")
 };
 
 UENUM(BlueprintType)
 enum ESQEX_Bonamik_WindType
 {
-	ESQEX_Bonamik_WindType_Directional = 0,
-	ESQEX_Bonamik_WindType_Wave = 1,
-	ESQEX_Bonamik_WindType_CustomWave = 2,
-	ESQEX_Bonamik_WindType_PointBlast = 3,
-	ESQEX_Bonamik_WindType_MAX = 4
+	ESQEX_Bonamik_WindType_Directional = 0 UMETA(DisplayName = "Directional"),
+	ESQEX_Bonamik_WindType_Wave = 1 UMETA(DisplayName = "Wave"),
+	ESQEX_Bonamik_WindType_CustomWave = 2 UMETA(DisplayName = "Custom Wave"),
+	ESQEX_Bonamik_WindType_PointBlast = 3 UMETA(DisplayName = "Point Blast"),
+	ESQEX_Bonamik_WindType_MAX = 4 UMETA(DisplayName = "MAX")
 };
 
 USTRUCT(BlueprintType)
