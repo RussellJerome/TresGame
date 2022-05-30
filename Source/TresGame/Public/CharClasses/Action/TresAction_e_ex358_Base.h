2d11c8204e0f2e43f6225febd6f27e714eb41cdb
Narknon <Narknon0@gmail.com>
2022-05-30 03:19:03 -0400

    Moves

---
+++ Source/TresGame/Public/CharClasses/Action/TresAction_e_ex358_Base.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "TresAction_e_ex358_Base.generated.h"
+
+UCLASS(BlueprintType)
+class UTresAction_e_ex358_Base : public UTresAction_XIIILB_Base {
+    GENERATED_BODY()
+};
