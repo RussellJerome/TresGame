f3062d9cba3791bc64d031d2061c8f855413756a
Narknon <Narknon0@gmail.com>
2022-05-30 15:50:05 -0400

    Updates

---
+++ Source/TresGame/Public/NPC/ActionDefinitions/SQEX_CharBase.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "TresActionDefinitionBase.h"
+#include "SQEX_CharBase.generated.h"
+
+
+UCLASS(HideDropdown)
+class UTresAction_e_he90y_Down : public UTresActionDefinitionBase
+{
+	GENERATED_BODY()
+}; // Size: 0xD0
+
+
+
+UCLASS(HideDropdown)
+class UTresAction1_e_ex044_ChangeHeight : public UTresActionDefinitionBase
+{
+	GENERATED_BODY()
+};
+
+
