1d7a985a466935554c157f220f7aa69a977a042e
Narknon <Narknon0@gmail.com>
2022-06-05 00:17:19 -0400

    Revert "Added Kinedriver and Bonamik Factories"
    This reverts commit 93b4af6370be422fcd85b517c7a024e1228a9212.

--- Source/KineDriver/KineDriverEd/Public/SQEX_KineDriverData_Factory.h
+++ Source/KineDriver/KineDriverEd/Public/SQEX_KineDriverData_Factory.h
@@ -4,19 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "Factories/Factory.h"
+#include "EditorReimportHandler.h"
 #include "SQEX_KineDriverData_Factory.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class KINEDRIVERRT_API USQEX_KineDriverData_Factory : public UFactory
+class KINEDRIVERED_API USQEX_KineDriverData_Factory : public UFactory, public FReimportHandler
 {
 	GENERATED_BODY()
-
-public:
+	
 	USQEX_KineDriverData_Factory();
 	virtual uint32 GetMenuCategories() const override;
 	FText GetDisplayName() const override { return FText::FromString(TEXT("KineDriver Data")); }
 	virtual UObject* FactoryCreateNew(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Cntext, FFeedbackContext* Warn) override;
+	virtual UObject* FactoryCreateBinary(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn, bool& bOutOperationCanceled) override;
+	virtual bool CanReimport(UObject* Obj, TArray<FString>& OutFilenames) override;
+	virtual void SetReimportPaths(UObject* Obj, const TArray<FString>& NewReimportPaths) override;
+	virtual EReimportResult::Type Reimport(UObject* Obj) override;
 };
