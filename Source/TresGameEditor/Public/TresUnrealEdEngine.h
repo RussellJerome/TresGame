cdfbce95cb854b0de203d818189c38a6b9ed74fe
Joseph Lee <17460257+josephl70@users.noreply.github.com>
2022-06-13 13:40:08 -0400

    Added DefaultEngine.ini

---
+++ Source/TresGameEditor/Public/TresUnrealEdEngine.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Editor/UnrealEdEngine.h"
+#include "TresUnrealEdEngine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRESGAMEEDITOR_API UTresUnrealEdEngine : public UUnrealEdEngine
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
