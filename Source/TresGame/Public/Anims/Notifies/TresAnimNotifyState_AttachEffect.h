7ac8dd473485362f20402658b5036203b788f21c
narknon <73571427+narknon@users.noreply.github.com>
2022-06-13 14:57:27 -0400

    Update TresAnimNotifyState_AttachEffect.h

--- Source/TresGame/Public/Anims/Notifies/TresAnimNotifyState_AttachEffect.h
+++ Source/TresGame/Public/Anims/Notifies/TresAnimNotifyState_AttachEffect.h
@@ -2,8 +2,6 @@
 #include "CoreMinimal.h"
 #include "TresAnimNotify_AttachEffect.h"
 #include "Animation/AnimNotifies/AnimNotifyState.h"
-//CROSS-MODULE INCLUDE V2: -ModuleName=Engine -ObjectName=SQEX_AttachEffectNotifyDataToMetaAttachData -FallbackName=SQEX_AttachEffectNotifyDataToMetaAttachData
-//CROSS-MODULE INCLUDE V2: -ModuleName=Engine -ObjectName=SQEX_AttachEffectNotifyDataToDataAsset -FallbackName=SQEX_AttachEffectNotifyDataToDataAsset
 #include "TresAnimNotifyState_AttachEffect.generated.h"
 
 UCLASS(CollapseCategories, EditInlineNew, MinimalAPI)
