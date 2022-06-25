cf9917aaea64cf61a9db78751a1734fa8754b9e5
Narknon <narknon0@gmail.com>
2022-06-25 16:58:35 -0400

    add anim stuff

--- Source/TresGame/Public/Anims/TresAnimAssetUnit.h
+++ Source/TresGame/Public/Anims/TresAnimAssetUnit.h
@@ -8,10 +8,10 @@ USTRUCT(BlueprintType)
 struct FTresAnimAssetUnit {
     GENERATED_BODY()
 public:
-    UPROPERTY(EditDefaultsOnly)
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     FName AnimName;
     
-    UPROPERTY(EditDefaultsOnly)
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     UAnimationAsset* AnimData;
     
     TRESGAME_API FTresAnimAssetUnit();
